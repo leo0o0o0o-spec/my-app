d1a51316a38004a4c6f9b5bf19e7f00ee99084a7
leo0o0o0o-spec <3571538198@qq.com>
2026-04-22 20:57:36 +0800

    feat: 配置生产环境 API 地址

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
 
-const api = axios.create({
-  baseURL: 'http://localhost:3001',
-})
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001'
 
+const api = axios.create({
+  baseURL: API_URL,
+})
 // 请求拦截器：自动带上 token
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
